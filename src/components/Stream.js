6a89e84a9b3c102b2f2cf475ca3d6239286e8142
Michael E <mickyfin10@hotmail.com>
2019-05-05 20:43:05 +0100

    layout sorted

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -27,19 +27,35 @@ class Stream extends React.Component {
     // const status = stateProps.status;
     // const status = "loading";
     return (
-      <div class="page">
-        <div>
-          <ReactTwitchEmbedVideo
-            channel="monstercat"
-            layout="video"
-            theme="dark"
-          />
+      <div className="page">
+        <div className="slimjim">
+          <div className="left-side">
+            <div className="headers">
+              <div className="link">Home</div>
+              <div className="link">About Me</div>
+              <img className="logo" src="https://i.imgur.com/rP1BZto.png" />
+              <div className="link">Feed</div>
+              <div className="link">Socials</div>
+            </div>
+            <div className="header">
+              Full-Time Dad, Developer and Entertainer
+            </div>
+            <ReactTwitchEmbedVideo
+              channel="monstercat"
+              layout="video"
+              theme="dark"
+              height="100%"
+              width="100%"
+            />
+          </div>
         </div>
-        <div>
+        <div className="chat">
           <Iframe
             id="ward1"
-            src="https://www.twitch.tv/embed/ward1/chat"
+            src="https://www.twitch.tv/embed/ward1/chat?darkpopout"
             width="100%"
+            height="100%"
+            frameBorder="0"
           />
         </div>
       </div>
