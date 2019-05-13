35cb57bf5b8d47bbefc0acf752f9106f3841c02c
michael eldred <mickyfin10@hotmail.com>
2019-05-13 15:02:13 +0100

    got the stickys working

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./navbar.css";
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div className="navbarbg">
+        <div className="navbarcontainer">
+          <div className="lefty">
+            <div className="navbarlogo">
+              <img className="logo" src="https://i.imgur.com/rP1BZto.png" />
+            </div>
+            <div className="navbarstreamer">{this.props.streamer}</div>
+          </div>
+          <div className="righty">
+            <div className="streamerlinks">
+              <div className="highlight">Follow</div>
+              <div>Subscribe</div>
+              <div>Tip</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default NavBar;
--- src/components/Stream.js
+++ src/components/Stream.js
@@ -3,6 +3,9 @@ import { getState } from "redux";
 import Loader from "./Loader";
 import axios from "axios";
 import Iframe from "react-iframe";
+import ReactPlayer from "react-player";
+import NavBar from "./Navbar";
+
 import "./stream.css";
 
 // import bg from "https://www.youtube.com/watch?v=KcUnXunuDTs&t=2127s";
@@ -12,33 +15,27 @@ import FetchSuccess from "../actions/FetchSuccess";
 import FetchFailure from "../actions/FetchFailure";
 import TwitchApp from "../reducers/TwitchApp";
 class Stream extends React.Component {
+  state = {
+    streamer: "danucd"
+  };
   componentDidMount() {
     this.props.store.subscribe(this.forceUpdate.bind(this));
     this.apiRequest();
     this.dispatchFetchRequest();
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    var options = {
-      width: 600,
-      height: 600,
-      channel: "ward1"
-    };
-    document.body.appendChild(script);
-
-    var player = Twitch.player("test", options);
   }
 
   apiRequest() {
     axios
-      .get("https://api.twitch.tv/helix/streams?user_login=danucd", {
-        headers: {
-          "Client-ID": "ffid0hjsu47c8dw5of4ejkt3grgrzy"
+      .get(
+        "https://api.twitch.tv/helix/streams?user_login=" + this.state.streamer,
+        {
+          headers: {
+            "Client-ID": "ffid0hjsu47c8dw5of4ejkt3grgrzy"
+          }
         }
-      })
+      )
       .then(response => {
-        console.log(response);
         const stream = "A";
-        console.log(response);
         this.dispatchFetchSuccess(stream);
       })
       .catch(e => {
@@ -66,49 +63,51 @@ class Stream extends React.Component {
     // ));
     // console.log(streamCardItems);
     return (
+      // <div clasName="pagewithnav">
       <div className="page">
-        <div className="slimjim">
-          <div className="left-side">
-            <div className="headers">
-              <img className="logo" src="https://i.imgur.com/rP1BZto.png" />
-              {/* <div className="link">Home</div>
-              <div className="link">About Me</div>
+        <div className="left-side">
+          <NavBar streamer={this.state.streamer} />
 
-              <div className="link">Feed</div>
-              <div className="link">Socials</div> */}
-            </div>
-            <div className="header">
-              Full-Time Dad, Developer and Entertainer
-            </div>
-            <div className="stream">
-              {status === "loading" ? (
-                <Loader />
-              ) : status === "success" ? (
-                // streamCardItems
-                <div id="test" />
-              ) : (
-                // <ReactTwitchEmbedVideo
-                //   channel="monstercat"
-                //   layout="video"
-                //   theme="dark"
-                //   height="100%"
-                //   width="100%"
-                // />
-                <div />
-              )}
-            </div>
+          <div className="stream">
+            {status === "loading" ? (
+              <Loader />
+            ) : status === "success" ? (
+              // streamCardItems
+              <div id="focused-item">
+                <ReactPlayer
+                  url={"https://www.twitch.tv/" + this.state.streamer}
+                  width="100%"
+                  height="100%"
+                />
+              </div>
+            ) : (
+              <div />
+            )}
+          </div>
+          <div className="panel-container">
+            <div className="panel">A</div>
+            <div className="panel">B</div>
+            <div className="panel">C</div>
+            <div className="panel">D</div>
+            <div className="panel">E</div>
+            <div className="panel">F</div>
           </div>
         </div>
         <div className="chat">
           <Iframe
-            id="ward1"
-            src="https://www.twitch.tv/embed/ward1/chat?darkpopout"
+            id={this.state.streamer}
+            src={
+              "https://www.twitch.tv/embed/" +
+              this.state.streamer +
+              "/chat?darkpopout"
+            }
             width="100%"
             height="100%"
             frameBorder="0"
           />
         </div>
       </div>
+      // </div>
     );
   }
 }
